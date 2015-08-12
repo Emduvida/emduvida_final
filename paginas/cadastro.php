e4855344dcfc49a8d230d83759d585b91e552d58
Alguém <igorcarlos1997@gmail.com>
2015-08-11 22:08:41 -0300

    terminando o front do front ehuehueh

---
+++ paginas/cadastro.php
@@ -0,0 +1,92 @@
+<section id="containerCadastro">
+
+    <article class="boxForm cancelaLeft">
+        <div class="conteudoBox">
+            <div class="contFormBox">
+                <h3 class="titulo-cadastro-home">Dados Pessoais</h3>
+                <hr class="linha-cadastro-home"/>
+
+                <form method="post" class="frmCadastro">
+                    <label for="nome" class="lblPadrao fonte">Nome:</label>
+                    <p class="erros"></p>
+                    <input type="text" name="" id="nome" class="frm-padrao input-cadastro-completo"/>  
+
+                    <label for="sobreNome" class="lblPadrao fonte">Sobrenome:</label>
+                    <p class="erros"></p>
+                    <input type="text" name="" id="sobreNome" class="frm-padrao input-cadastro-completo"/>  
+
+                    <label for="data" class="lblPadrao fonte">Data de nascimento:</label>
+                    <p class="erros"></p>
+                    <input type="text" name="" id="data" class="frm-padrao input-cadastro-completo"/>  
+
+                    <label for="cpf" class="lblPadrao fonte">Data de nascimento:</label>
+                    <p class="erros"></p>
+                    <input type="text" name="" id="cpf" class="frm-padrao input-cadastro-completo"/>  
+
+                </form>
+
+            </div>
+        </div>
+        <div id="bottomBox1" class="bottom">
+
+
+
+        </div>
+    </article>
+
+    <article class="boxForm">
+        <div class="conteudoBox">
+            <div class="contFormBox">
+                <h3 class="titulo-cadastro-home">Localização</h3>
+                <hr class="linha-cadastro-home"/>
+
+                <form method="post" class="frmCadastro">
+                    <label for="cidade" class="lblPadrao fonte">Cidade:</label>
+                    <p class="erros"></p>
+                    <input type="text" name="" id="cidade" class="frm-padrao input-cadastro-completo"/>  
+
+                    <label for="estado" class="lblPadrao fonte">Estado:</label>
+                    <p class="erros"></p>
+                    <input type="text" name="" id="estado" class="frm-padrao input-cadastro-completo"/>  
+
+                </form>
+
+            </div>
+        </div>
+        <div id="bottomBox2" class="bottom"></div>
+    </article>
+
+    <article class="boxForm right">
+        <div class="conteudoBox">
+            <div class="contFormBox">
+                <h3 class="titulo-cadastro-home">Confirmar</h3>
+                <hr class="linha-cadastro-home"/>
+
+                <form method="post" class="frmCadastro">
+                    <label for="email" class="lblPadrao fonte">Email:</label>
+                    <p class="erros"></p>
+                    <input type="text" name="" id="email" class="frm-padrao input-cadastro-completo"/>  
+
+                    <label for="senha" class="lblPadrao fonte">Senha:</label>
+                    <p class="erros"></p>
+                    <input type="text" name="" id="senha" class="frm-padrao input-cadastro-completo"/>  
+
+                    <h3 class="titulo-cadastro-home">Mais</h3>
+                    <hr class="linha-cadastro-home"/>
+
+
+                    <div id="carregaImagem"></div>
+
+
+                    <div id="msgTermos">
+                        <input type="checkbox">
+                        Declaro que li e aceito os termos de 
+                        uso do site “EM DUVIDA”.</div>
+                    <input type="submit" name="" value="Cadastrar" class="btnCadastrar"/>
+                </form>
+
+            </div>
+        </div>
+        <div id="bottomBox3" class="bottom"></div>
+    </article>
+</section>
