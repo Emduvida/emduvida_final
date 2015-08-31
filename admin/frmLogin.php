6ee3d49c0a48a3199a04896154968005460de62e
equipe4_3a <equipe4_3a@LAB1M33.itbeng.inf>
2015-08-31 11:26:57 -0300

    opa "o admin voltou"

--- admin/frmLogin.php
+++ admin/frmLogin.php
@@ -8,10 +8,10 @@
         <label for="email" class="lblPadrao">E-mail</label>
         <input type="email" id="email" name="email" />
 
-        <label for="senha">Senha</label>
+        <label for="senha" class="lblPadrao">Senha</label>
         <input type="email" id="senha" name="email" />
 
-        <input type="submit" name="cadastrar"/>
+        <input type="submit" class="inputPadrao" name="cadastrar"/>
     </form>
 
 </html>
